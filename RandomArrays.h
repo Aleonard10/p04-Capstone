1a055ad8b18db744e1ddd40ea6ada29523ed7051
Aleonard10 <31227161+Aleonard10@users.noreply.github.com>
2017-12-07 20:30:33 -0800

    Class Function declarations and private variables

---
+++ RandomArrays.h
@@ -0,0 +1,16 @@
+#ifndef RANDOMARRAYS_H
+#define RANDOMARRAYS_H
+
+class RandomArrays
+{
+    public:
+        int getArrayValue(int i);
+        int checkArray(int x);
+        RandomArrays();
+    private:
+        int y;
+        int array[10];
+        int randomNumber;
+        int sortedArray[10];
+};
+#endif
